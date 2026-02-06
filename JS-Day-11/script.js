617e54f49c799c7fb8366f3a539219ac71752820
Daksh-cyberx <jha067475@gmail.com>
2026-02-06 12:37:34 +0530

    Added JS-Day-11

---
+++ JS-Day-11/script.js
@@ -0,0 +1,23 @@
+// function handleClick(){
+//     const button = document.getElementById("btn");
+//     button.textContent = "Clicked"
+// }
+
+// function handleClick(){
+//     const heading = document.querySelector("h1");
+//     heading.style.color = "red";
+//     const button = document.getElementById("btn");
+//     button.textContent = "color changed"
+// }
+
+function handleClick(){
+    const newpara = document.createElement("h2");
+    newpara.textContent = "this is a new h2 tag";
+    
+
+    const image = document.createElement("img");
+    image.setAttribute("src", "https://d3lzcn6mbbadaf.cloudfront.net/media/details/ANI-20240624063839.jpeg")
+    image.setAttribute("alt", "K.R Mangalam University")
+    document.getElementById("content").append(newpara);
+    document.getElementById("content").append(image);
+}

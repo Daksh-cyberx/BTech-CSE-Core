a037ae7ef8bd0c3432c74675c9482878de4c4cb4
Daksh-cyberx <jha067475@gmail.com>
2026-02-23 23:32:18 +0530

    updated JS-Day-10

--- JS-Day-10/script.js
+++ JS-Day-10/script.js
@@ -1,35 +1,35 @@
-// const para = document.getElementsByTagName("p")
+const para = document.getElementsByTagName("p")
 
-// for(let i=0;i<para.length;i++){
-//     if(i%2===0){
-//         para[i].style.color="blue";
-//     }else{
-//         para[i].style.color="green";
-//     }
-// }
-// para[para.length-1].style.fontWeight="bold"
+for (let i = 0; i < para.length; i++) {
+    if (i % 2 === 0) {
+        para[i].style.color = "blue";
+    } else {
+        para[i].style.color = "green";
+    }
+}
+para[para.length - 1].style.fontWeight = "bold"
 
 
 // from now on we have to use forEach method...
-// para.forEach((para, index)=>{
-//     if(index %2===0){
-//         para.style.color="blue";
-//     }else{
-//         para.style.color="green";
-//     }
-// })
+para.forEach((para, index) => {
+    if (index % 2 === 0) {
+        para.style.color = "blue";
+    } else {
+        para.style.color = "green";
+    }
+})
 
 // create a new element and append it
 const newpara = document.createElement("p")
-newpara.textContent="This is a dynamically created paragraph.";
+newpara.textContent = "This is a dynamically created paragraph.";
 console.log(newpara)
 
 document.getElementById("content").append(newpara);
 
 
-const image=document.createElement("img");
-image.setAttribute("src","https://d3lzcn6mbbadaf.cloudfront.net/media/details/ANI-20240624063839.jpeg")
-image.setAttribute("alt","K.R Mangalam University")
+const image = document.createElement("img");
+image.setAttribute("src", "https://d3lzcn6mbbadaf.cloudfront.net/media/details/ANI-20240624063839.jpeg")
+image.setAttribute("alt", "K.R Mangalam University")
 
 document.getElementById("gallery").append(image);
 

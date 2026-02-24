66ebee92c48fe3c1216c981eaecd8f2bbbb6be12
Daksh-cyberx <jha067475@gmail.com>
2026-02-24 23:54:28 +0530

    study event handling

---
+++ JS-Day-13/script.js
@@ -0,0 +1,23 @@
+function handleClick() {
+    alert("clicked");
+};
+
+const btn = document.getElementById("btn");
+btn.onclick = function () {
+    this.textContent = "Clicked!";
+    this.style.color = "green";
+};
+
+const button = document.getElementById("hello");
+
+// function myFunction(){                   // this is called using named function method....
+//     console.log("clicked");
+// };
+
+// button.addEventListener("click", myFunction);
+
+button.addEventListener("click", function(event){          // this is called using anonymous function method....
+    console.log("clicked");
+    console.log(event.target);
+})
+

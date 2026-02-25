6fe3ba2a0d2a8869e62bf0cb03714e882f6949c0
Daksh-cyberx <jha067475@gmail.com>
2026-02-25 23:53:07 +0530

    revise function and it's types

---
+++ Function/script.js
@@ -0,0 +1,7 @@
+function greet(){
+    console.log("hello!");
+};
+greet();// it prints the "hello!" in the console but does not return anything means undefined.
+
+let result = greet();
+console.log(result);

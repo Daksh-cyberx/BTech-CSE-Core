9f7f694b9dc9607f03f2963b461c893f1669b8c9
Daksh-cyberx <jha067475@gmail.com>
2026-03-04 22:51:30 +0530

    updated JS-Day-13

--- JS-Day-13/script.js
+++ JS-Day-13/script.js
@@ -1,4 +1,4 @@
-console.log("a");  //synchronos code
+console.log("a");  //synchronos code - it block the next line code
 setTimeout(()=>{       // asynchronos code - it does not block the next line code , runs simultaneously with another code
     console.log("b")   // but in console, first execute will be synchronos code, then asynchronos code 
 }, 5000);
@@ -9,3 +9,20 @@ setTimeout(()=>{
     console.log("d")
 }, 2000);
 console.log("e");
+
+// callback hell:                            //This pyramid like structure called "Pyramid of Doom" in which
+setTimeout(()=>{                             //Disadvantages of Callback hell:
+    console.log("a");                        //1. appearance is bad
+    setTimeout(()=>{                         //2. difficulty in debugging in code
+        console.log("b");                    //3. hard to read
+        setTimeout(()=>{                     
+            console.log("c");
+            setTimeout(()=>{
+                console.log("d");
+                setTimeout(()=>{
+                    console.log("e");
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000);

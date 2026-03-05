bfa3682eb7ddc7e8e839761e49dc107b8c90e157
Daksh-cyberx <jha067475@gmail.com>
2026-03-05 10:56:37 +0530

    updated JS-Day-14

--- JS-Day-14/script.js
+++ JS-Day-14/script.js
@@ -1,15 +1,60 @@
 // 1. setTimeout() :-
 //setTimeout() is used to execute a function after some specified delay.
 
-console.log("hiiii")
-let timer1 = setTimeout(() => {
-    console.log("hello")
-}, 3000);
+// console.log("hiiii")
+// let timer1 = setTimeout(() => {
+//     console.log("hello")
+// }, 3000);
 
-let timer = setInterval(() => {
-    console.log("heheheh")
-}, 4000);
-console.log(timer);
+// let timer = setInterval(() => {
+//     console.log("heheheh")
+// }, 4000);
+// console.log(timer);
 
-clearTimeout(timer1);
+// clearTimeout(timer1);
 
+// //Topic:- Promise
+
+// let pro1 = new Promise((resolve, reject) => {
+//     let b = 11;
+//     if (b>20){
+//         resolve();
+//     }else{
+//         reject();
+//     }
+// })
+// pro.then(()=>console.log("promise resolved"))
+// .catch(()=>console.log("promise rejected"))
+
+
+// function pro(){
+//     return new Promise((resolve, reject) => {
+//         let a = 10;
+//         if (a>50){
+//             resolve();
+//         }else{
+//             reject()
+//         }
+//     })
+// }
+// pro()
+// .then(()=>console.log("promise resolved"))
+// .catch(()=>console.log("promise rejected"))
+// .finally(()=>console.log("inside finally"))
+
+
+//guess the output and explain the reason behind it..
+console.log("a")
+setTimeout(()=>{
+    console.log("b")
+}, 0)
+setTimeout(()=>{
+    console.log("c")
+}, 5000)
+Promise.resolve().then(()=>{
+    console.log("promise")
+})
+setTimeout(()=>{
+    console.log("d")
+}, 2000)
+console.log("e")
